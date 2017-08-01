340c1723f6b8c3599175519112449a39d3c79f11
Virmerson <virmerson@hightechcursos.com.br>
2017-07-31 21:39:21 -0400

    buscas com Spring Data JPA

--- src/main/java/br/ucdb/UsuarioController.java
+++ src/main/java/br/ucdb/UsuarioController.java
@@ -1,23 +1,61 @@
 package br.ucdb;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * Created by Virmerson Bento dos Santos on 7/24/17.
  */
 
-@RestController("/usuarios")
+@RestController
 public class UsuarioController {
 
     @Autowired
     private UsuarioRepository usuarioRepository;
 
-    @PostMapping
-    public Usuario salvar(@RequestBody Usuario usuario) {
-        return usuarioRepository.save(usuario);
+    @PostMapping("/usuarios")
+    public Usuario cadastrar(@RequestBody Usuario usuario) {
+        return salvar(usuario);
     }
+
+    @PutMapping("/usuarios")
+    public Usuario alterar(@RequestBody Usuario usuario) {
+        return salvar(usuario);
+    }
+
+
+    private Usuario salvar(Usuario usuario) {
+       return usuarioRepository.save(usuario);
+    }
+
+    @GetMapping("/usuarios/todos")
+    public List<Usuario> buscarTodos(){
+        return usuarioRepository.findAll();
+    }
+
+    @GetMapping("/usuarios/nome")
+    public List<Usuario> buscarPorNome(@RequestParam("nome") String nome){
+        return usuarioRepository.findByNomeIgnoreCase(nome);
+    }
+
+    @GetMapping("/usuarios/email")
+    public List<Usuario> buscarPorEmail(@RequestParam("email") String email){
+        return usuarioRepository.findByEmail(email);
+    }
+
+    @GetMapping("/usuarios/nomeemail")
+    public List<Usuario> buscarPorEmail(@RequestParam("nome") String nome, @RequestParam("email") String email){
+        return usuarioRepository.findByNomeOrEmailAllIgnoreCase(nome, email);
+    }
+
+    @DeleteMapping("/usuarios")
+    public void excluir (@RequestBody Usuario usuario){
+         usuarioRepository.delete(usuario);
+    }
+
+
 
 }
--- src/main/java/br/ucdb/UsuarioRepository.java
+++ src/main/java/br/ucdb/UsuarioRepository.java
@@ -2,8 +2,26 @@ package br.ucdb;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * Created by Virmerson Bento dos Santos on 7/24/17.
  */
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+    List<Usuario> findByNome(String nome);
+
+    //Select * From usuarios where upper(nome) = upper(?)
+    List<Usuario> findByNomeIgnoreCase(String nome);
+
+    List<Usuario> findByEmail(String email);
+
+    List<Usuario> findByNomeAndEmail(String nome, String email);
+
+    List<Usuario> findByNomeIgnoreCaseAndEmailIgnoreCase(String nome, String email);
+
+    List<Usuario> findByNomeAndEmailAllIgnoreCase(String nome, String email);
+
+    List<Usuario> findByNomeOrEmailAllIgnoreCase(String nome, String email);
+
 }
